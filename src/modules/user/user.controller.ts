ef0405c15868de0be83576588fe5259e373c237c
Matheus <matheus-rodrigues37@live.com>
2023-04-17 17:18:56 -0300

    MED-53 feat: use guard in all routes and add update password route

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -5,21 +5,24 @@ import {
   HttpStatus,
   Request,
   UseGuards,
+  Body,
 } from '@nestjs/common';
 import { ApiOperation, ApiTags, ApiResponse } from '@nestjs/swagger';
 import {
   IProfileRequest,
   IProfileResponse,
 } from '@common/interfaces/userProfileResponses';
+import { IServerResponse } from '@common/interfaces/serverResponses';
 import { AuthGuard } from '@nestjs/passport';
+import { UpdateUserDto } from '@modules/user/dto/updateUser.dto';
 import { UserService } from './user.service';
 
+@UseGuards(AuthGuard('jwt'))
 @ApiTags('user')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @UseGuards(AuthGuard('jwt'))
   @Get('profile')
   @ApiOperation({ summary: 'Request user profile' })
   @ApiResponse({
@@ -38,7 +41,6 @@ export class UserController {
     };
   }
 
-  @UseGuards(AuthGuard('jwt'))
   @Patch('update')
   @ApiOperation({ summary: 'Update user profile' })
   @ApiResponse({
@@ -54,6 +56,35 @@ export class UserController {
     return {
       statusCode: HttpStatus.OK,
       data: user,
+    };
+  }
+
+  @Patch('update-password')
+  @ApiOperation({ summary: 'Update user password' })
+  @ApiResponse({
+    status: 200,
+    description: 'User password updated successfully',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'User password request was rejected',
+  })
+  async updatePassword(
+    @Request() req: IProfileRequest,
+    @Body() dto: UpdateUserDto,
+  ): Promise<IServerResponse> {
+    await this.userService.updatePassword(
+      {
+        email: req.user?.email,
+      },
+      {
+        currentPassword: dto.currentPassword,
+        newPassword: dto.newPassword,
+      },
+    );
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Password was updated',
     };
   }
 }
